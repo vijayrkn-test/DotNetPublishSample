68ee463ded84bff51576cd3704219ee5e7b4d981
Vijay Ramakrishnan <vramak@microsoft.com>
2016-07-01 14:27:01 -0700

    Sample csproj containing publish target prototypes

---
+++ DotNetPublishSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetPublishSample.Startup))]
+namespace DotNetPublishSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
